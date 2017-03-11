409ecb515c770281a65fe6be98fce71d01c6cc0f
Hassina GADA <hassina-gada@outlook.fr>
2017-03-11 15:05:01 +0100

    Completed Utils.java tests

--- test/utils/UtilsTest.java
+++ test/utils/UtilsTest.java
@@ -2,6 +2,9 @@ package utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -17,17 +20,23 @@ class UtilsTest {
 
     @Test
     void celsiusToKelvin() {
-
+        assertTrue(Utils.celsiusToKelvin(2041.85).equals(2315.0));
+        assertTrue(Utils.celsiusToKelvin(-296.15).equals(-23.0));
+        assertTrue(Utils.celsiusToKelvin(-273.15).equals(0.0));
     }
 
     @Test
     void kelvinToCelsius1() {
-
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(2315.0, -23.0, 0.0));
+        ArrayList<Double> b = new ArrayList<>(Arrays.asList(2041.85, -296.15, -273.15));
+        assertTrue(Utils.kelvinToCelsius(a).equals(b));
     }
 
     @Test
     void celsiusToKelvin1() {
-
+        ArrayList<Double> a = new ArrayList<>(Arrays.asList(2315.0, -23.0, 0.0));
+        ArrayList<Double> b = new ArrayList<>(Arrays.asList(2041.85, -296.15, -273.15));
+        assertTrue(Utils.celsiusToKelvin(b).equals(a));
     }
 
 }

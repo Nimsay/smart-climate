1983b236b4ff36dd65af5b8a8387232ebd224d8a
Yasmin AIT MAKSENE <yasmin.aitmak@gmail.com>
2017-03-13 21:02:15 +0100

    added year list initialization

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -5,37 +5,69 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ToggleButton;
-import parser.MeteoFrance;
+import parser.CSV;
 
+import parser.MeteoFrance;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 
 /**
  * Created by Thunderstorm on 19/02/17.
  */
+
+
+
+
 public class MainController {
-    @FXML LineChart<Integer, Integer> lineChart;
-    @FXML CheckBox compareCheckbox;
-    @FXML ChoiceBox year1, month1, day1;
-    @FXML ChoiceBox year2, month2, day2;
+
+
+
+    @FXML
+    LineChart<String, Number> lineChart;
+    @FXML
+    CheckBox compareCheckbox;
+    @FXML
+    ChoiceBox year1, month1, day1;
+    @FXML
+    ChoiceBox year2, month2, day2;
     @FXML ChoiceBox aggMode;
     @FXML ToggleButton yearMode, monthMode, dayMode;
 
-    final Integer minimumYear = 1996;
+    private ObservableList<String> yearsOList;
+    private ArrayList<String>  yearsList,monthsList;
+    private Calendar cal;
+    private MeteoFrance meteoFrance ;
+    final private Integer METEO_FRANCE_MIN_YEAR = 1996;
 
-    @FXML void initialize(){
+    public MainController() throws IOException {
+
+        this.meteoFrance = new MeteoFrance();
+
+        //init year list based on actual date
+        yearsList = new ArrayList<>();
+        cal = Calendar.getInstance();
+        for (int year = cal.get(Calendar.YEAR); year >= METEO_FRANCE_MIN_YEAR; year--){
+            yearsList.add(String.valueOf(year));
+        }
+        yearsOList = FXCollections.observableArrayList(yearsList);
+
+    }
+        @FXML
+    void initialize() {
         // populate
         Calendar cal = Calendar.getInstance();
         int currentYear = cal.get(Calendar.YEAR);
 
         ArrayList<String> availableYears = new ArrayList<>();
-        for (int year=currentYear; year >= minimumYear; year--){
+        for (int year = currentYear; year >= METEO_FRANCE_MIN_YEAR; year--) {
             availableYears.add(String.valueOf(year));
         }
 
@@ -46,19 +78,20 @@ public class MainController {
         year2.getSelectionModel().selectFirst();
 
 
-        aggMode.setItems( FXCollections.observableArrayList( "Moyenne", "MinMax", "Ecart Type") );
+        aggMode.setItems(FXCollections.observableArrayList("Moyenne", "MinMax", "Ecart Type"));
         aggMode.getSelectionModel().selectFirst();
 
     }
 
-    @FXML public void onCompare(ActionEvent event){
-        if (compareCheckbox.isSelected()){
+    @FXML
+    public void onCompare(ActionEvent event) {
+        if (compareCheckbox.isSelected()) {
             year2.setDisable(false);
 
-            if ( dayMode.isSelected() || monthMode.isSelected() ){
+            if (dayMode.isSelected() || monthMode.isSelected()) {
                 month2.setDisable(false);
             }
-            if (dayMode.isSelected()){
+            if (dayMode.isSelected()) {
                 day2.setDisable(false);
             }
         } else {
@@ -68,15 +101,18 @@ public class MainController {
         }
     }
 
-    @FXML public void onKelvinSelected(){
+    @FXML
+    public void onKelvinSelected() {
         System.out.println("Kelvin Selected");
     }
 
-    @FXML public void onCelsiusSelected(){
+    @FXML
+    public void onCelsiusSelected() {
         System.out.println("Celsius Selected");
     }
 
-    @FXML public void onYearMode(ActionEvent event){
+    @FXML
+    public void onYearMode(ActionEvent event) {
         year1.setDisable(false);
         month1.setDisable(true);
         day1.setDisable(true);
@@ -87,7 +123,8 @@ public class MainController {
         }
     }
 
-    @FXML public void onMonthMode(ActionEvent event){
+    @FXML
+    public void onMonthMode(ActionEvent event) {
         year1.setDisable(false);
         month1.setDisable(false);
         day1.setDisable(true);
@@ -98,7 +135,8 @@ public class MainController {
         }
     }
 
-    @FXML public void onDayMode(ActionEvent event){
+    @FXML
+    public void onDayMode(ActionEvent event) {
         year1.setDisable(false);
         month1.setDisable(false);
         day1.setDisable(false);
@@ -109,15 +147,11 @@ public class MainController {
         }
     }
 
-//    @FXML public void mainDatePickerActionHandler(ActionEvent event) throws IOException {
-//        MeteoFrance meteofrance = new MeteoFrance();
-//        LocalDate selectedLocalDate = mainDatePicker.getValue();
-//        String selectedDate = selectedLocalDate.getYear() + String.format("%02d", selectedLocalDate.getMonthValue() );
-//        System.out.println("Selected Month: " + selectedDate);
-//        ArrayList<Integer> data = meteofrance.get(selectedDate, "temperature");
-//    }
-
-
-
 }
+
+
+
+
+
+
 

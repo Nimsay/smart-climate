9ca188e6128614b1707417710b7e300901030b82
Hassina GADA <hassina-gada@outlook.fr>
2017-04-22 21:26:30 +0100

    Utils updated

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,36 +1,53 @@
 package utils;
 
-import java.lang.reflect.Array;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
-
-/**
- * Created by hassina on 27/02/2017.
- */
+import java.util.zip.GZIPInputStream;
 
 public class Utils {
+
+    /**
+     * Gunzip prend un fichier gz en entrée et donne en sortie un fichier decompresse
+     * @param inputGzFile nom de fichier d'entree en gz format
+     * @param outputFile nom de fichier de sortie
+     * @return true si ça c'est decompresse correctement. false sinon
+     */
+    public static boolean gunzip(String inputGzFile, String outputFile){
+
+        byte[] buffer = new byte[1024];
+
+        try{
+            GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(inputGzFile));
+            FileOutputStream out = new FileOutputStream(outputFile);
+
+            int len;
+            while ((len = gzis.read(buffer)) > 0) {
+                out.write(buffer, 0, len);
+            }
+            gzis.close();
+            out.close();
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
     public static Double kelvinToCelsius(Double kelvin){
         return kelvin-273.15;
     }
 
-    public static Double celsiusToKelvin (Double celsius){
-        return celsius+273.15;
-    }
+    public static ArrayList<Pair> kelvinToCelsius(ArrayList<Pair>kelvins){
 
-    public static ArrayList<Double> kelvinToCelsius(ArrayList<Double> kelvins){
-
-        ArrayList<Double> results = new ArrayList<>();
-        for (Double kelvin: kelvins){
-           results.add(kelvinToCelsius(kelvin));
-        }
-        return results;
-    }
-
-    public static ArrayList<Double> celsiusToKelvin(ArrayList<Double> celsius){
-
-        ArrayList<Double> results = new ArrayList<>();
-        for (Double celsiu: celsius){
-            results.add(celsiusToKelvin(celsiu));
+        ArrayList<Pair> results = new ArrayList<>();
+        for (Pair kelvinPair: kelvins){
+            results.add(new Pair(
+                    kelvinPair.getKey(),
+                    String.valueOf(kelvinToCelsius(Double.valueOf(kelvinPair.getValue()))))
+            );
         }
         return results;
     }

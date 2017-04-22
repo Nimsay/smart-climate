cef8db81728ff9bfc3982831a7eae65f3251f5c7
Hassina GADA <hassina-gada@outlook.fr>
2017-04-22 22:19:52 +0100

    changed structure

---
+++ src/source/UILists.java
@@ -0,0 +1,13 @@
+package source;
+
+
+import utils.Pair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public interface UILists {
+
+    ArrayList<Pair> getStationsList() throws IOException;
+}
